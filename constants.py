d1e0a01efa0430afd697c51c6abe85899ad3fe69
mahdi <mahdi.rezaie.336@gmail.com>
2021-07-17 19:15:38 +0430

    Create constants class

---
+++ constants.py
@@ -0,0 +1,13 @@
+class Consts:
+
+    NEGATIVE_DATASET = './rt-polarity.neg'
+    POSITIVE_DATASET = './rt-polarity.pos'
+
+    LOWER_FREQUENCY_CUTOFF = 2
+    UPPER_FREQUENCY_CUTOFF = 5
+
+    EPSILON = 0.1
+    LAMBDA_1 = 0.3
+    LAMBDA_2 = 0.3
+    LAMBDA_3 = 0.4
+
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import re
+from constants import Consts
+
 
 neg = {}
 pos = {}
 neg_bi = {}
 pos_bi = {}
-low_frequency_threshold = 2
-most_repeated_remove = 5
 num_of_pos = 0
 num_of_neg = 0
 
@@ -20,18 +20,28 @@ def count_unigram(unigram: dict):
     return sum(unigram.values())
 
 
+def probability(w, unigram: dict, unigram_length: int):
+    return unigram[w] / unigram_length
+
+
+def conditional_probability(w1, w2, bigram: dict, unigram: dict):
+    if unigram[w2] > 0:
+        return bigram[w1][w2] / unigram[w2]
+    return 0.0
+
+
 def cleanse_unigram(unigram: dict):
     to_remove = []
 
     # Remove words with number of repeats less than threshold
     for word in unigram:
-        if unigram[word] <= low_frequency_threshold:
+        if unigram[word] <= Consts.LOWER_FREQUENCY_CUTOFF:
             to_remove.append(word)
     for i in to_remove:
         del unigram[i]
 
     # Remove top n repeated words
-    for i in range(most_repeated_remove):
+    for i in range(Consts.UPPER_FREQUENCY_CUTOFF):
         maximum = 0
         max_word = ''
         for word in unigram:
@@ -47,13 +57,15 @@ def cleanse_bigram(bigram: dict):
     # Remove words with number of repeats less than threshold
     for first_word in bigram:
         for second_word in bigram[first_word]:
-            if bigram[first_word][second_word] <= low_frequency_threshold:
+            if bigram[first_word][second_word] <= Consts.LOWER_FREQUENCY_CUTOFF:
                 to_remove.append((first_word, second_word))
     for i in to_remove:
         del bigram[i[0]][i[1]]
+        if len(bigram[i[0]]) == 0:
+            del bigram[i[0]]
 
     # Remove top n repeated words
-    for i in range(most_repeated_remove):
+    for i in range(Consts.UPPER_FREQUENCY_CUTOFF):
         maximum = 0
         max_word = ('', '')
         for first_word in bigram:
@@ -62,11 +74,13 @@ def cleanse_bigram(bigram: dict):
                     maximum = bigram[first_word][second_word]
                     max_word = (first_word, second_word)
         del bigram[max_word[0]][max_word[1]]
+        if len(bigram[max_word[0]]) == 0:
+            del bigram[max_word[0]]
 
 
 def read_training_datasets():
     # Reading Negative Dataset
-    with open('./rt-polarity.neg', 'r') as file:
+    with open(Consts.NEGATIVE_DATASET, 'r') as file:
         for line in file:
             prev_word = ''
             for word in pre_process_filter(line):
@@ -77,7 +91,7 @@ def read_training_datasets():
                 prev_word = word
 
     # Reading Positive Dataset
-    with open('./rt-polarity.pos', 'r') as file:
+    with open(Consts.POSITIVE_DATASET, 'r') as file:
         for line in file:
             prev_word = ''
             for word in pre_process_filter(line):
@@ -93,7 +107,7 @@ def read_training_datasets():
     cleanse_bigram(neg_bi)
     cleanse_bigram(pos_bi)
 
-    # Count number of negative and positive words
+    # Counting number of negative and positive words
     global num_of_neg, num_of_pos
     num_of_neg = count_unigram(neg)
     num_of_pos = count_unigram(pos)
@@ -101,6 +115,8 @@ def read_training_datasets():
 
 def main():
     read_training_datasets()
+    for i in neg_bi:
+        print(i, neg_bi[i])
 
 
 if __name__ == '__main__':
